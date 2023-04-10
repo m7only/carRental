bb4cb8bf22fcc1e5e3cbea64326ac1ed61a1ceb9
Anton Skryagin <44167136+m7only@users.noreply.github.com>
2023-04-10 07:22:58 +0700

    Init commit

---
+++ src/test/java/m7/only/carrental/CarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package m7.only.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
